c851caa85dc6566b617c5deb4f426aa95c10bd7b
zexin.guo <zexin.guo@qunar.com>
2018-11-06 14:37:30 +0800

    初步完成

--- src/main/java/com/baobao/HiveJdbcDemo.java
+++ src/main/java/com/baobao/HiveJdbcDemo.java
@@ -6,13 +6,14 @@ import java.sql.*;
 
 /**
  * 需要先把当前系统用户jessin加入到hadoop的/usr/local/hadoop-2.7.3/etc/hadoop/core-site.xml中。
- * 在/home/jessin/Downloads/apache-hive-2.3.3-bin/conf/hive-site.xml中配置用户zhangsan
+ * 在/home/jessin/Downloads/apache-hive-2.3.3-bin/conf/hive-site.xml中配置用户jessin
+ * 注意jessin与启动hadoop的linux 账号一样，避开一些权限问题。
  * <p>
  * 1. 启动hadoop，查看9000端口是否启动：start-dfs.sh
  * 2. 启动hiveserver2，命令：hiveserver2
  * 3. 查看hive.log：tail -f /tmp/jessin/hive.log
  * 4. 使用hive命令查看hive数据：hive
- * 或者beeline -u jdbc:hive2://localhost:10000 -n zhangsan -p 123456789
+ * 或者beeline -u jdbc:hive2://localhost:10000 -n jessin -p 123456789
  *
  * @author zexin.guo
  * @create 2018-11-05 下午8:58
@@ -32,34 +33,18 @@ public class HiveJdbcDemo {
             System.exit(1);
         }
 
-        Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/myhive", "zhangsan", "123456789");
+        Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/myhive", "jessin", "123456789");
 
         Statement stmt = con.createStatement();
-        String tableName = "student";
-        // TODO 如何设置权限
-//        stmt.execute("drop table if exists " + tableName);
-//        stmt.execute("create table " + tableName +
-//                " (key int, value string)");
-//        System.out.println("Create table success!");
-//        // show tables
-        String sql = "show tables '" + tableName + "'";
-        ResultSet res = stmt.executeQuery(sql);
-        if (res.next()) {
-            LOG.info("表存在：{}", res.getString(1));
-        }
+        String tableName = "hello";
 
-        // describe table
-        sql = "describe " + tableName;
-        res = stmt.executeQuery(sql);
-        while (res.next()) {
-            LOG.info("{}\t{}", res.getString(1), res.getString(2));
-        }
+//        init(stmt, tableName);
 
-        sql = "select * from " + tableName;
-        LOG.info("Running: {}", sql);
-        res = stmt.executeQuery(sql);
-        while (res.next()) {
-            LOG.info("{}\t{}", String.valueOf(res.getInt(1)), res.getString(2));
+        String selectSql = "select * from " + tableName;
+        LOG.info("Running: {}", selectSql);
+        ResultSet selectSqlResult = stmt.executeQuery(selectSql);
+        while (selectSqlResult.next()) {
+            LOG.info("{}\t{}", selectSqlResult.getString(1), selectSqlResult.getInt(2));
         }
 
         // TODO select count(*)会导致OOM..
@@ -69,5 +54,22 @@ public class HiveJdbcDemo {
 //        while (res.next()) {
 //            LOG.info("执行结果：{}", res.getString(1));
 //        }
+    }
+
+    private static void init(Statement stmt, String tableName) throws SQLException {
+        // TODO 如何设置权限
+        stmt.execute("drop table if exists " + tableName);
+        stmt.execute("create table " + tableName +
+                "(key String, value int)" +
+                "row format delimited fields terminated by ',' stored as textfile");
+
+        String showTableSql = "show tables '" + tableName + "'";
+        ResultSet showTableRes = stmt.executeQuery(showTableSql);
+        if (showTableRes.next()) {
+            LOG.info("表存在：{}", showTableRes.getString(1));
+        }
+
+        String loadDataSql = "load data local inpath '/home/jessin/kv.csv' into table  " + tableName;
+        stmt.executeUpdate(loadDataSql);
     }
 }
